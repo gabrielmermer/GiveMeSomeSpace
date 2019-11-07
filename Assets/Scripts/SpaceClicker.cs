2c99a28a81598b62b4f99933611b970de50d25ef
zubrzysta <zubrzysta@gmail.com>
2019-11-07 09:38:56 +0100

    Initial commit

---
+++ Assets/Scripts/SpaceClicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SpaceClicker : MonoBehaviour
+{
+    public Text SpaceText;
+    int SpaceClickCounter;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpaceClickCounter = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        DisplayGreetMessage();
+
+        CountSpacePressed();
+    }
+
+    private void CountSpacePressed()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpaceText.text = SpaceClickCounter.ToString();
+            SpaceClickCounter += 1;
+        }
+    }
+
+    private void DisplayGreetMessage()
+    {
+        if (SpaceClickCounter == 0)
+        {
+            SpaceText.text = "Press SPACE";
+        }
+    }
+}
---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    public Text TimeText;
+    [SerializeField] int minTimeRange;
+    [SerializeField] int maxTimeRange;
+    int GameTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // generates random game time to play
+        GameTime = Random.Range(minTimeRange, maxTimeRange);
+        InvokeRepeating("TimeUpdate", 0, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeUpdate();
+
+    }
+
+    private void TimeUpdate()
+    {
+        if (GameTime >= 0)
+        {
+            TimeText.text = GameTime.ToString();
+            Invoke("RemoveOneFromGameTime", 1);
+            GameTime = GameTime - 1;
+        }
+        else
+        {
+            TimeText.text = "TIME OUT";
+        }
+    }
+}
